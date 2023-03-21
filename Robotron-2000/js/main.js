bdb9853e433d7fccea767244ae1522ca67b9f61e
Yindi Torres <yindiyft@gmail.com>
2023-03-21 19:48:14 -0300

    Adicionando estatisticas de acordo com os atributos de cada peca

--- Robotron-2000/js/main.js
+++ Robotron-2000/js/main.js
@@ -2,14 +2,50 @@
 const subtrair = document.querySelector('#subtrair')
 const somar = document.querySelector('#somar')
 
+const controle = document.querySelectorAll('[data-controle]')
+const estatisticas = document.querySelectorAll('[data-estatistica]')
+//console.log(estatisticas)
 
-const controle = document.querySelectorAll('.controle-ajuste')
+const pecas = {
+    "bracos": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
+
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos":{
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -24
+    },
+    "pernas":{
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 42
+    },
+    "foguetes":{
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
+    }
+}
 
 //Adiciona evendo ManipulaDados quando ocorrer o evento click em todos elementos da classe controle-ajuste
 controle.forEach((elemento) => {
     elemento.addEventListener('click', (evento)=>{
-        manipulaDados(evento.target.textContent, evento.target.parentNode)//manipula o conteudo do texto da target
+        manipulaDados(evento.target.dataset.controle, evento.target.parentNode)//manipula o conteudo do texto da target
         //console.log(evento.target.parentNode)//encontra o elemento pai na herança, o qual iremos referenciar
+        atualizaEstatisticas(evento.target.dataset.peca) //Atualiza a funcao atualizaEstatisticas com a informacao da peça que for clicada
     })
     
 })
@@ -27,6 +63,15 @@ function manipulaDados(operacao, controle){
     } 
 }
 
+//ATUALIZAR AS ESTATÍSTICAS COM OS DADOS DAS PEÇAS:
+function atualizaEstatisticas(peca){
+    console.log(pecas[peca])
+
+    estatisticas.forEach((elemento) => {
+        elemento.textContent = parseInt(elemento.textContent) + pecas[peca][elemento.dataset.estatistica]
+        console.log()
+    })
+}
 
 
 
