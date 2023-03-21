46af5dc7b055e0cfaefbbae089340335faa0ef7d
Yindi Torres <yindiyft@gmail.com>
2023-03-20 23:07:32 -0300

    Adicionando eventos ao clicar com o foreach

--- Robotron-2000/js/main.js
+++ Robotron-2000/js/main.js
@@ -5,11 +5,18 @@ const braco = document.querySelector('#braco')
 
 const controle = document.querySelectorAll('.controle-ajuste')
 
-somar.addEventListener("click", (evento) => {manipulaDados('somar')} )
-subtrair.addEventListener("click", (evento) => {manipulaDados('subtrair')})
+controle.forEach((elemento) => {
+    elemento.addEventListener('click', (evento)=>{
+        manipulaDados(evento.target.textContent)
+    })
+    
+})
+
+// somar.addEventListener("click", (evento) => {manipulaDados('somar')} )
+// subtrair.addEventListener("click", (evento) => {manipulaDados('subtrair')})
 
 function manipulaDados(operacao){
-    if(operacao === 'subtrair'){
+    if(operacao === '-'){
         braco.value = parseInt(braco.value) -1; //o valor do braço deve ser transformado para inteiro, pois é uma string (00)
     }else{
         braco.value = parseInt(braco.value) +1;
